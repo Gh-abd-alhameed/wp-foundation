bc4ad280280f1cd473f738fd0295f8662cda0091
Gh-abd-alhameed <gh.abd.alhameed.97@gmail.com>
2022-11-22 18:46:02 +0200

    hook action and filter

---
+++ boca/core/settings/Hooks.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace boca\core\settings;
+
+class Hooks
+{
+	public static $hook;
+	public static $actions;
+	public static $filters;
+
+	public static function Init(string $hook, \Closure $callback)
+	{
+		self::$hook = $hook;
+		$callback();
+		self::runAction();
+		self::runFilter();
+	}
+
+	public static function filter(\Closure $callback)
+	{
+		self::$filters[] = ["hook" => self::$hook, "callback" => $callback];
+	}
+
+	public static function action(\Closure $callback)
+	{
+		self::$actions[] = ["hook" => self::$hook, "callback" => $callback];
+	}
+	public static function runAction()
+	{
+		foreach (self::$actions as $key => $value)
+		{
+			add_action($value["hook"] , $value["callback"]);
+		}
+	}
+	public static function runFilter()
+	{
+		foreach (self::$filters as $key => $value)
+		{
+			add_filter($value["hook"] , $value["callback"]);
+		}
+	}
+}
+
